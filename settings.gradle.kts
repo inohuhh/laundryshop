edc73b9e32bf9e0168c76dfc354de383ac90e47b
inohuhh <161554864+inohuhh@users.noreply.github.com>
2024-05-02 17:47:22 +0800

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "admin"
+include(":admin")
+include(":staff")
+include(":customer")
